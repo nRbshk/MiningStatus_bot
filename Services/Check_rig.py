aed4b0e40ec0f0efb69c18006d93e25177412927
nRbshk <n.ryaboshchuk@gmail.com>
2021-05-19 02:16:56 +0300

    change info printing

--- Services/Check_rig.py
+++ Services/Check_rig.py
@@ -6,11 +6,11 @@ from aiogram.utils.markdown import code, bold
 from aiogram import types
 
 
-from Helpers.helpers import config, prepare_message
+from Helpers.helpers import config, prepare_message, save_config
 
 from aiogram import Bot
 
-from requests import get, post
+from requests import get
 
 
 logger = logging.getLogger(__name__)
@@ -25,6 +25,7 @@ async def check_rig(bot: Bot):
         response = get(url)
 
         if response.status_code != 200:
+            await bot.edit_message_text("No active miners!", config['chat_id'], config['last_message'],)
             await sleep(delay)
             continue
         
@@ -34,8 +35,12 @@ async def check_rig(bot: Bot):
         start_time = json['start_time']
 
         edited_msg = code(prepare_message(json))
-
-        await bot.edit_message_text(edited_msg, config['chat_id'], config['last_message'], parse_mode=types.ParseMode.MARKDOWN_V2)
+        if config['last_message'] == "-1":
+            last_message = await bot.send_message(chat_id=config['chat_id'], text=edited_msg, parse_mode=types.ParseMode.MARKDOWN_V2)
+            config['last_message'] = last_message['message_id']
+            save_config(config)
+        else:
+            await bot.edit_message_text(edited_msg, config['chat_id'], config['last_message'], parse_mode=types.ParseMode.MARKDOWN_V2)
 
 
 
