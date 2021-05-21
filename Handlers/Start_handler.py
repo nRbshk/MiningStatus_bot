0a57a0be108a65731fe9862066cfd07a397becb5
nRbshk <n.ryaboshchuk@gmail.com>
2021-05-21 11:50:58 +0300

    Only admin can control the rigs.

--- Handlers/Start_handler.py
+++ Handlers/Start_handler.py
@@ -22,10 +22,7 @@ async def cmd_cancel(message: types.Message, state: FSMContext):
 
 async def start_h(message: types.Message, state: FSMContext):
     cid = message.from_user.id
-    if config['chat_id'] != cid:
-        await message.answer("You are not admin and you can't use this bot.")
-        await state.finish()
-    elif config['chat_id'] == '-1':
+    if config['chat_id'] == "-1":
         last_message = await message.answer(f"Config file succesfully updated.\nIf config file is configured it will update automaticaly.")
         logger.info(f"Message from user {cid}")
 
@@ -35,6 +32,10 @@ async def start_h(message: types.Message, state: FSMContext):
         save_config(config)
         
         await state.finish()
+    elif config['chat_id'] != cid:
+        await message.answer("You are not admin and you can't use this bot.")
+        await state.finish()
+
 
 def register_handlers_start(dp: Dispatcher):
     dp.register_message_handler(start_h, commands="start", state="*")
