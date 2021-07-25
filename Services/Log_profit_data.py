6549d193910b50f7ba7b8fd635f21d06e92dfc8d
nRbshk <n.ryaboshchuk@gmail.com>
2021-07-25 19:04:18 +0300

    add logger for profit

---
+++ Services/Log_profit_data.py
@@ -0,0 +1,36 @@
+import logging
+
+from asyncio import sleep
+
+
+from Helpers.helpers import check_balance_at_nanopool, config, get_current_profit
+
+from Services.DB import db
+
+logger = logging.getLogger(__name__)
+
+
+
+async def log_data():
+    coin_name = 'ETH'
+    while True:
+        balance = check_balance_at_nanopool(coin_name.lower(), config['WALLET'][coin_name.lower()]).split("\n")
+        if len(balance) == 1:
+            continue
+        balance = balance[1].split(" ")[-1]
+
+        profit_rewards = get_current_profit(coin_name)
+
+        if profit_rewards == 1:
+            continue
+        day_profit, day_reward = profit_rewards
+
+        db.insert_balance_profit(coin_name, balance, day_profit, day_reward)
+
+
+
+        await sleep(30)
+
+
+
+
